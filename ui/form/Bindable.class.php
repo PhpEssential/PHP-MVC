5581c81e8321b77b183a223253bc281dd352e89a
Vince <offspring_13@hotmail.fr>
2019-03-02 18:10:22 +0100

    Review binding mechanism

--- ui/form/Bindable.class.php
+++ ui/form/Bindable.class.php
@@ -1,14 +1,14 @@
 <?php
 namespace framework\ui\form;
 
-use framework\Config;
 use framework\exception\IllegalArgumentException;
 use framework\internationalization\Message;
 use framework\utils\FileUtils;
 use framework\utils\LogUtils;
+use framework\utils\StringUtils;
 
 /**
- * Classe permettant de définir un objet comme étant assignable depuis une requète HTTP
+ * You can bind an object with data of an http request if it inherit this class
  */
 abstract class Bindable {
 	const TYPE_DATE = 0;
@@ -17,159 +17,199 @@ abstract class Bindable {
 	const TYPE_INT = 3;
 	const TYPE_FLOAT = 4;
 	const TYPE_BOOL = 5;
-	const TYPE_ARRAY = 6;
-	const TYPE_FILE = 7;
-	const TYPE_BINDABLE = 8;
 
 	/**
-	 * Permet d'assigner les attributs de l'objet et de validé les données
+	 * Start loading of bindable object's attributes
 	 *
 	 * @param array $data
-	 *        	Données provenant de la requête HTTP
+	 *        	- Data from http request $_POST or $_GET or $_FILE or $_POST + $_FILE or ...
+	 * @return bool
 	 */
 	public function bind(array $data): bool {
 		return $this->fill($data) && $this->validate();
 	}
 
+	/**
+	 * Validation of loaded attribute values
+	 *
+	 * @return bool
+	 */
 	protected function validate(): bool {
 		return true;
 	}
 
+	/**
+	 * Load attributes of the bindable object
+	 *
+	 * @param array $data
+	 *        	- Data from http request $_POST or $_GET or $_FILE or $_POST + $_FILE or ...
+	 * @return bool
+	 */
 	protected abstract function fill(array $data): bool;
 
-	protected function bindObjectListField(string $fieldName, array $data, string $class, bool $required = false): bool {
-		$this->$fieldName = null;
+	protected function bindObjectListField(string $fieldName, array $data, string $class, $default = array()): bool {
 		$valid = true;
-
-		try {
-			if ($required && isset($data [$fieldName])) {
+		if (isset($data [$fieldName])) {
+			try {
 				$this->$fieldName = array ();
 				foreach ( $data [$fieldName] as $itemData ) {
 					$item = new $class();
 					$valid = $item->bind($itemData) && $valid;
 					$this->$fieldName [] = $item;
+					if (! $valid)
+						break;
 				}
-			} else if (isset($data [$fieldName])) {
-				$this->$fieldName = array ();
-				foreach ( $data [$fieldName] as $itemData ) {
-					$item = new $class();
-					$valid = $item->bind($itemData) && $valid;
-					$this->$fieldName [] = $item;
-				}
-			} else if ($required) {
-				$valid = false;
+			} catch ( \Exception $e ) {
+				$valid = $this->handleError($e, $fieldName);
 			}
-		} catch ( \Exception $e ) {
-			LogUtils::error($e);
-			$valid = false;
-		}
-		return $valid;
-	}
-
-	protected function bindObjectField(string $fieldName, array $data, string $class, bool $required = false): bool {
-		$this->$fieldName = null;
-		$valid = true;
-
-		try {
-			if ($required && isset($data [$fieldName])) {
-				$this->$fieldName = new $class();
-				$valid = $this->$fieldName->bind($data [$fieldName]);
-			} else if (isset($data [$fieldName])) {
-				$this->$fieldName = new $class();
-				$valid = $this->$fieldName->bind($data [$fieldName]);
-			} else if ($required) {
-				$valid = false;
-			} else {
-				$this->$fieldName = new $class();
-			}
-		} catch ( \Exception $e ) {
-			LogUtils::error($e);
-			$valid = false;
-		}
-		return $valid;
-	}
-
-	protected function bindListField(string $fieldName, array $data, int $dataType, bool $required = false): bool {
-		$this->$fieldName = array ();
-		$valid = true;
-		$present = false;
-
-		try {
-			if ($required && isset($data [$fieldName])) {
-				$valid = true;
-				$present = true;
-			} else if (isset($data [$fieldName])) {
-				$valid = true;
-				$present = true;
-			} else if ($required) {
-				$valid = false;
-			}
-
-			if ($valid && $present) {
-				$this->$fieldName = array ();
-				foreach ( $data [$fieldName] as $item ) {
-					$this->$fieldName [] = $this->parseValue($item, $dataType);
-				}
-			}
-		} catch ( \Exception $e ) {
-			LogUtils::error($e);
-			$valid = false;
-		}
-		return $valid;
-	}
-
-	protected function bindField(string $fieldName, array $data, int $dataType, bool $required = false): bool {
-		$this->$fieldName = null;
-		$valid = true;
-
-		try {
-			if ($required && isset($data [$fieldName])) {
-				$this->$fieldName = $this->parseValue($data [$fieldName], $dataType);
-				$valid = true;
-			} else if (isset($data [$fieldName])) {
-				$this->$fieldName = $this->parseValue($data [$fieldName], $dataType);
-				$valid = true;
-			} else if ($required) {
-				$valid = false;
-			}
-		} catch ( \Exception $e ) {
-			LogUtils::error($e);
-			$valid = false;
-		}
-		return $valid;
-	}
-
-	protected function bindFile(string $fieldName, array $data, array $allowedExt, int $fileSize, bool $required = false): bool {
-		$this->$fieldName = null;
-		$valid = true;
-
-		try {
-			if ($required && isset($data [$fieldName])) {
-				$file = $data [$fieldName];
-				FileUtils::checkFile($file, $allowedExt, $fileSize);
-				$this->$fieldName = $file;
-				$valid = ($file != null);
-			} else if (isset($data [$fieldName])) {
-				$file = $data [$fieldName];
-				FileUtils::checkFile($file, $allowedExt, $fileSize);
-				$this->$fieldName = $file;
-				$valid = ($file != null);
-			} else if ($required) {
-				$valid = false;
-			}
-		} catch ( \Exception $e ) {
-			LogUtils::error($e);
-			$valid = false;
+		} else {
+			$this->$fieldName = $default;
 		}
 		return $valid;
 	}
 
 	/**
+	 * Bind object attribute from request
+	 *
+	 * @param string $fieldName
+	 *        	- Name of field to load
+	 * @param array $data
+	 *        	- Data from http request $_POST or $_GET or $_FILE or $_POST + $_FILE or ...
+	 * @param string $class
+	 *        	- Class of object to load
+	 * @param mixed $default
+	 *        	- Default value (null by default)
+	 *
+	 * @return bool - TRUE if no problem
+	 */
+	protected function bindObjectField(string $fieldName, array $data, string $class, $default = null): bool {
+		$valid = true;
+		if (isset($data [$fieldName])) {
+			$reducedData = $data [$fieldName];
+		} else {
+			$reducedData = array_reduce(array_keys($data), function ($acc, $item) use ($fieldName, $data) {
+				if (StringUtils::startsWith($item, $fieldName . "_")) {
+					$acc [substr($item, strlen($fieldName) + 1)] = $data [$item];
+				}
+				return $acc;
+			}, array ());
+		}
+
+		if (! empty($reducedData)) {
+			try {
+				$this->$fieldName = new $class();
+				$valid = $this->$fieldName->bind($reducedData);
+			} catch ( \Exception $e ) {
+				$valid = $this->handleError($e, $fieldName);
+			}
+		} else {
+			$this->$fieldName = $default;
+		}
+		return $valid;
+	}
+
+	/**
+	 * Bind list of primitive attribute from request
+	 *
+	 * @param string $fieldName
+	 *        	- Name of field to load
+	 * @param array $data
+	 *        	- Data from http request $_POST or $_GET or $_FILE or $_POST + $_FILE or ...
+	 * @param string $dataType
+	 *        	- Data type
+	 * @param mixed $default
+	 *        	- Default value (array() by default)
+	 *
+	 * @return bool - TRUE if no problem
+	 */
+	protected function bindListField(string $fieldName, array $data, int $dataType, $default = array()): bool {
+		$valid = true;
+		if (isset($data [$fieldName])) {
+			try {
+				$this->$fieldName = array ();
+				foreach ( $data [$fieldName] as $item ) {
+					$this->$fieldName [] = $this->parseValue($item, $dataType);
+				}
+			} catch ( \Exception $e ) {
+				$valid = $this->handleError($e, $fieldName);
+			}
+		} else {
+			$this->$fieldName = $default;
+		}
+		return $valid;
+	}
+
+	/**
+	 * Bind primitive attribute from request
+	 *
+	 * @param string $fieldName
+	 *        	- Name of field to load
+	 * @param array $data
+	 *        	- Data from http request $_POST or $_GET or $_FILE or $_POST + $_FILE or ...
+	 * @param string $dataType
+	 *        	- Data type
+	 * @param mixed $default
+	 *        	- Default value (null by default)
+	 *
+	 * @return bool - TRUE if no problem
+	 */
+	protected function bindField(string $fieldName, array $data, int $dataType, $default = null): bool {
+		$valid = true;
+		if (isset($data [$fieldName])) {
+			try {
+				$this->$fieldName = $this->parseValue($data [$fieldName], $dataType);
+			} catch ( \Exception $e ) {
+				$valid = $this->handleError($e, $fieldName);
+			}
+		} else {
+			$this->$fieldName = $default;
+		}
+
+		return $valid;
+	}
+
+	/**
+	 * Bind file attribute from request
+	 *
+	 * @param string $fieldName
+	 *        	- Name of field to load
+	 * @param array $data
+	 *        	- Data from http request $_FILE or $_POST + $_FILE or ...
+	 * @param string $allowedExt
+	 *        	- Allowed MIME types
+	 * @param mixed $fileSize
+	 *        	- Max allowed file size
+	 *
+	 * @return bool - TRUE if no problem
+	 */
+	protected function bindFile(string $fieldName, array $data, array $allowedExt, int $fileSize): bool {
+		$valid = true;
+		if (isset($data [$fieldName])) {
+			try {
+				$file = $data [$fieldName];
+				FileUtils::checkFile($file, $allowedExt, $fileSize);
+				$this->$fieldName = $file;
+				$valid = ($file != null);
+			} catch ( \Exception $e ) {
+				$valid = $this->handleError($e, $fieldName);
+			}
+		} else {
+			$this->$fieldName = null;
+		}
+		return $valid;
+	}
+
+	/**
+	 * Parse value from http request
 	 *
 	 * @param mixed $value
+	 *        	- Value from request
 	 * @param int $dataType
-	 * @throws IllegalArgumentException
-	 * @return mixed
+	 *        	- Data type
+	 *
+	 * @throws IllegalArgumentException - If unable to parse request data
+	 *
+	 * @return mixed - The parsed data
 	 */
 	private function parseValue($value, int $dataType) {
 		switch ($dataType) {
@@ -185,18 +225,24 @@ abstract class Bindable {
 				return strval($value);
 			case self::TYPE_BOOL :
 				return $value == "true" || $value == "on";
-			case self::TYPE_FILE :
-				$uploaddir = Config::get(Config::UPLOAD_DIR);
-				$fileName = basename($_FILES ['userfile'] ['name']);
-				$uploadfile = $uploaddir . $fileName;
-				if (move_uploaded_file($_FILES ['userfile'] ['tmp_name'], $uploadfile)) {
-					return $fileName;
-				} else {
-					throw new IllegalArgumentException(Message::ERROR_UNEXPECTED);
-				}
-				return $value;
 			default :
 				throw new IllegalArgumentException(Message::get(Message::ERROR_BAD_FORMAT));
 		}
+	}
+
+	/**
+	 * Handle binding error
+	 *
+	 * @param \Exception $e
+	 *        	- Thrown exception
+	 * @param string $fieldName
+	 *        	- Current loaded field
+	 *
+	 * @return bool FALSE
+	 */
+	private function handleError(\Exception $e, string $fieldName): bool {
+		LogUtils::error($e);
+		$this->$fieldName = null;
+		return false;
 	}
 }
