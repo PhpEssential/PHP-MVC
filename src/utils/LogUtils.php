cc8a799a50c528916a3d0e83afe0001a49178ade
TheSideEffect <offspring_13@hotmail.fr>
2019-04-14 16:06:41 +0200

    log error jusst string

--- src/utils/LogUtils.php
+++ src/utils/LogUtils.php
@@ -1,32 +1,39 @@
 <?php
+
 namespace phpessential\mvc\utils;
+
+use \Exception;
 
 class LogUtils {
 
-	public static function error(\Exception $e) {
-		$str = self::logError($e, "\n");
-		error_log($str);
-	}
+    public static function error($e) {
+        if($e instanceof Exception) {
+            $str = self::logError($e, "\n");
+        } else {
+            $str .= "[ERROR] " . $e . "\n";
+        }
+        error_log($str);
+    }
 
-	public static function info(string $message) {
-		error_log("[INFO] " . $message);
-	}
+    public static function info(string $message) {
+        error_log("[INFO] " . $message);
+    }
 
-	public static function warning(string $message) {
-		error_log("[WARNING] " . $message);
-	}
+    public static function warning(string $message) {
+        error_log("[WARNING] " . $message);
+    }
 
-	public static function debug(string $message) {
-		error_log("[DEBUG] " . $message);
-	}
+    public static function debug(string $message) {
+        error_log("[DEBUG] " . $message);
+    }
 
-	private static function logError(\Exception $e, $str = "") {
-		$str .= "[ERROR] " . $e->getMessage() . "\n";
-		$str .= "[ERROR] " . $e->getTraceAsString() . "\n";
-		if ($e->getPrevious() != null) {
-			$str = self::logError($e->getPrevious(), $str);
-		}
-		return $str;
-	}
+    private static function logError(Exception $e, $str = "") {
+        $str .= "[ERROR] " . $e->getMessage() . "\n";
+        $str .= "[ERROR] " . $e->getTraceAsString() . "\n";
+        if ($e->getPrevious() != null) {
+            $str = self::logError($e->getPrevious(), $str);
+        }
+        return $str;
+    }
+
 }
-
