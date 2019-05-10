d31da0f82cfd19e3501e3d5458b2e74a2cbfc72a
PhpEssential <vincent-thibaut@live.fr>
2019-05-10 22:43:17 +0200

    Add controller management in framework

---
+++ src/controllers/Controller.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace phpessential\mvc\controllers;
+
+/**
+ * Base controller class, used for handle http request
+ */
+class Controller {
+
+    /**
+     * Send HTTP 500
+     */
+    protected function internalServerError() {
+        header("HTTP/1.1 500 Internal server error", true, 500);
+        exit();
+    }
+
+    /**
+     * Send HTTP 404
+     */
+    protected function notFound() {
+        header("HTTP/1.1 404 Not Found", true, 404);
+        exit();
+    }
+
+    /**
+     * Send HTTP 401
+     */
+    protected function unauthorized() {
+        header("HTTP/1.1 401 Unauthorized", true, 401);
+        exit();
+    }
+
+    /**
+     * Send HTTP 400
+     */
+    protected function badRequest() {
+        header("HTTP/1.1 400 Bad request", true, 400);
+        exit();
+    }
+
+    /**
+     * Redirect to specifc route
+     *
+     * @param string $route
+     *        	route vers laquelle rediriger
+     */
+    protected function redirection(string $route) {
+        header("Location:" . $route);
+        exit();
+    }
+
+    /**
+     * Send json to client
+     *
+     * @param mixed $objectToSend
+     *        	objet à transformer en json et à envoyer au client
+     */
+    protected function sendJson($objectToSend) {
+        echo json_encode($objectToSend);
+        exit();
+    }
+
+}
